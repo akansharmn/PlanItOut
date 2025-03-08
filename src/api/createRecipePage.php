236ead43c33ef6e9711a767d5986d94ebac26563
akansharaman23 <39585346-akansharaman23@users.noreply.replit.com>
2025-03-08 11:25:47 +0000

    Checkpoint before assistant change: Update HTMX version and fix header/footer inclusion in home and createRecipe pages.  Also, added logging to form reset script.

--- src/api/createRecipePage.php
+++ src/api/createRecipePage.php
@@ -1,10 +1,11 @@
 <?php
 namespace PlanItOut\Api;
 
-//include 'src/templates/header.php';
+include 'src/templates/header.php';
 
 // Include the createRecipePage.htmx content
 $createRecipeContent = file_get_contents('src/api/createRecipePage.htmx');
 echo $createRecipeContent;
 ?>
 
+<?php include 'src/templates/footer.php'; ?>
--- src/templates/header.php
+++ src/templates/header.php
@@ -8,24 +8,26 @@
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Include HTMX from CDN -->
-    <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLfu/aRVyM" crossorigin="anonymous"></script>
+   
+                                                                                                                              <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
     <!-- Material Icons (keeping for icon support) -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
 <!-- HTMX script for form reset -->
  <script>
-  // document.addEventListener('htmx:afterSettle', function(event) {
-  //   // Check if the resetForm event was triggered
-  //   const triggerHeader = event.detail.xhr && event.detail.xhr.getResponseHeader('HX-Trigger');
-  //   if (triggerHeader) {
-  //     const triggers = JSON.parse(triggerHeader);
-  //     if (triggers.resetForm) {
-  //       // Find the form and reset it
-  //       const form = document.querySelector('form[hx-post="/createRecipe"]');
-  //       if (form) form.reset();
-  //     }
-  //   }
-  // });
+   document.addEventListener('htmx:afterSettle', function(event) {
+     // Check if the resetForm event was triggered
+     const triggerHeader = event.detail.xhr && event.detail.xhr.getResponseHeader('HX-Trigger');
+     if (triggerHeader) {
+     const triggers = JSON.parse(triggerHeader);
+         Logger::debug(triggers);
+      if (triggers.resetForm) {
+         // Find the form and reset it
+         const form = document.querySelector('form[hx-post="/createRecipe"]');
+         if (form) form.reset();
+       }
+     }
+  });
 </script>
 
     <style>
