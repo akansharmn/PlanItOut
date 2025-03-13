395432a8fd8a39cbd2fd3c68c0485b80ccc35294
akansharmn <39441904+akansharmn@users.noreply.github.com>
2025-03-13 17:47:11 +0000

    Restored to '909e3eb8e1fb343727f3190786acae9b127ee4a5'
    Replit-Restored-To: 909e3eb8e1fb343727f3190786acae9b127ee4a5

--- src/api/home.php
+++ src/api/home.php
@@ -4,7 +4,8 @@ namespace PlanItOut\Api;
 include 'src/templates/header.php';
 
 // Include the home.htmx content
-include 'src/api/home.htmx';
-
-include 'src/templates/footer.php';
+$homeHtmxContent = file_get_contents('src/api/home.htmx');
+echo $homeHtmxContent;
 ?>
+
+<?php  include 'src/templates/footer.php'; ?>
